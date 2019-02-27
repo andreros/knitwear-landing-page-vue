4c0d8ffc146037b349dce9c54e6b0cd4a7147993
Andre Rosa <andre.rosa@namecheap.com>
2019-02-27 16:12:22 +0000

    Mobile Menu

--- src/components/page-header/index.vue
+++ src/components/page-header/index.vue
@@ -5,10 +5,19 @@
 			<div>
 				<router-link to="/" class="page-header__logo">{{brandName}}</router-link>
 			</div>
-			<div class="page-header__nav_links_wrapper">
-				<router-link to="/about" class="page-header__nav_link">About</router-link>
-				<router-link to="/contact" class="page-header__nav_link">Contact</router-link>
-				<router-link to="/shop" class="page-header__nav_link button">Shop</router-link>
+			<a ref="menu" class="page-header__nav_menu" @click="toggleMenu">
+				<i class="fas fa-bars"></i>
+			</a>
+			<div ref="linksContainer" class="page-header__nav_links_container">
+				<div class="page-header__nav_link_wrapper" @click="toggleMenu">
+					<router-link to="/about" class="page-header__nav_link">About</router-link>
+				</div>
+				<div class="page-header__nav_link_wrapper" @click="toggleMenu">
+					<router-link to="/contact" class="page-header__nav_link">Contact</router-link>
+				</div>
+				<div class="page-header__nav_link_wrapper" @click="toggleMenu">
+					<router-link to="/shop" class="page-header__nav_link button">Shop</router-link>
+				</div>
 			</div>
 		</nav>
 		<div class="page-header__copy">
@@ -29,6 +38,17 @@ export default {
 		punchline: {
 			type: String,
 			default: ''
+		}
+	},
+	methods: {
+		toggleMenu() {
+			if (this.$refs.menu.style.display !== 'none') {
+				this.$refs.menu.style.display = 'none';
+				this.$refs.linksContainer.style.display = 'flex';
+			} else {
+				this.$refs.menu.style.display = 'flex';
+				this.$refs.linksContainer.style.display = 'none';
+			}
 		}
 	}
 };
